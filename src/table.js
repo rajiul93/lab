8537427b81f56101b0a5a6e9b66221cad74492a9
rajiul93 <rajiulislam283@gmail.com>
2023-08-10 23:04:01 +0600

    second

--- src/table.js
+++ src/table.js
@@ -1,31 +1,26 @@
 import React, { useState } from "react";
 import data2 from "./program/data.json";
 export default function Table() {
-
   const [filterProduct, setFilterProduct] = useState("");
 
-  
-
   return (
-    <div className="mx-auto overflow-scroll  bg-slate-400 max-h-screen relative">
-            <div className="bg-slate-700   p-10" action="">
-   
-    <div className=" sticky top-2">
-      <div className="mb-3 flex gap-8">
-        <label htmlFor="" className="text-white text-2xl">
-          Chemical Name
-        </label>
-        <input
-          onChange={(event) => {
-            setFilterProduct(event.target.value);
-          }}
-          type="text"
-          className=" ps-3 py-1 text-xl"
-        />
-
+    <div className="overflow-scroll  bg-slate-400 max-h-screen relative">
+      <div className="bg-slate-700   p-10" action="">
+        <div className=" sticky top-2">
+          <div className="mb-3 flex gap-8">
+            <label htmlFor="" className="text-white text-2xl">
+              Chemical Name
+            </label>
+            <input
+              onChange={(event) => {
+                setFilterProduct(event.target.value);
+              }}
+              type="text"
+              className=" ps-3 py-1 text-xl"
+            />
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
       <table>
         <tr>
           <th>id</th>
@@ -34,32 +29,37 @@ export default function Table() {
           <th>specification</th>
           <th>Quantity</th>
         </tr>
-        {data2.filter((val) => {
+        {data2
+          .filter((val) => {
             if (filterProduct === "") {
               return val;
-            } 
-            else if(val.name2.toLowerCase().includes(filterProduct.toLowerCase())) {
-                return val
-            }
-            else{
-              return null
+            } else if (
+              val.name2.toLowerCase().includes(filterProduct.toLowerCase())
+            ) {
+              return val;
+            } else {
+              return null;
             }
           })
-          .map((val) => {
+          .map((val,ind) => {
             return (
               <tr
-                key={val.id}
+                key={ind}
                 className="border-solid hover:rounded-lg hover:bg-black hover:text-white border-2 border-sky-500 bores text-center"
               >
                 <td className="w-8 border-r-1 ">{val.id}</td>
                 <td className="w-48 text-left">{val.name1}</td>
                 <td className="w-48 text-left">{val.name2}</td>
                 <td className="w-48 text-left">{val.specification}</td>
-                <td className="w-48 text-center flex justify-around">{val.quantity}<button className="w-12 p-2 rounded-lg bg-lime-900">Edit</button></td>
+                <td className="w-48 text-center flex justify-around">
+                  {val.quantity}
+                  <button className="w-12 p-2 rounded-lg bg-lime-900">
+                    Edit
+                  </button>
+                </td>
               </tr>
             );
           })}
-       
       </table>
     </div>
   );
